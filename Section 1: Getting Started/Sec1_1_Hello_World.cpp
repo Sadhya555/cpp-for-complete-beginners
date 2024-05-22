d21b36e110fdfce470ab352363581fcc8176c2a2
Sannidhya Roy Sadhya <72967411+Sadhya555@users.noreply.github.com>
2024-05-23 01:44:37 +0600

    Added files

---
+++ Section 1: Getting Started/Sec1_1_Hello_World.cpp
@@ -0,0 +1,9 @@
+// Not exactly following the Udemy course
+// Avoiding using namespace, endl, flush, etc for the time being
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
---
+++ Section 1: Getting Started/Sec1_2_Outputting_Text.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Starting program...";
+    std::cout << " This is some text.\n";
+    std::cout << "Banana. Apple. Orange.\n";
+    std::cout << "This is some more text.\n";
+}
